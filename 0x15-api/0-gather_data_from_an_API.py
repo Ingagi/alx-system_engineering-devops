d55e694c5da63e6ed2ad6ea9b669b688698abfd3
Ingagi <oshumbusho3@gmail.com>
2024-12-24 04:53:25 -0800

    let's do this

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
-"""For a given employee ID, returns information about
-their TODO list progress"""
+""" Gather data from an API """
 
 import requests
-import sys
+from sys import argv
+"""modules to work with"""
+
+def get_employee_todos_progress(employee_id):
+    try:
+        url = "https://jsonplaceholder.typicode.com/"
+        user_datas = requests.get9url + f"users/(employee_id)")
+        user_data = user_datas.json()
+        employee_name = user_data['name']
+
+        """fetch todos list for the employee"""
+        todo_list = requests.get(url + f"todos?userId-(employee_id)")
+        json_todos_list = len(task_done)
+
+        """display the result"""
+        print(f"employee {employee_name} is done with tasks("
+                f"{no_task_done}/{total_task}):")
+
+        """title of completed task"""
+        for task in task_done:
+            print(f"\t {task['title']}")
+    except Exception as e:
+        print(f"an error occured: {e}:)
+
 
 if __name__ == "__main__":
+    if len(argv) != 2:
+        print("usage: script <employee_id>")
+        else:
+        get_eployee_todos_progress(argv[1])
 
-    userId = sys.argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                        .format(userId))
-
-    name = user.json().get('name')
-
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-    totalTasks = 0
-    completed = 0
-
-    for task in todos.json():
-        if task.get('userId') == int(userId):
-            totalTasks += 1
-            if task.get('completed'):
-                completed += 1
-
-    print('Employee {} is done with tasks({}/{}):'
-          .format(name, completed, totalTasks))
-
-    print('\n'.join(["\t " + task.get('title') for task in todos.json()
-          if task.get('userId') == int(userId) and task.get('completed')]))
